8cb8f163116378409e2138a76a6800c1cf1ae67b
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-09-03 15:15:39 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,8 +14,9 @@ var KonomiCode = [24, 24, 25, 25, 27, 26, 27, 26, 98, 97];
 function init() {
   let index = 0;
   document.body.addEventListener('keydown', function(event) {
-    if (event.key.charCodeAt() === 97) {
+    if (event.keyCode === 37) {
       alert('bingo');
     }
   });
 }
+init();
