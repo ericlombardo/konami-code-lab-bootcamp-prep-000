e701974cf6cd37fdb72e772adc68ae0f3379744a
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-09-03 15:58:31 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,17 +11,23 @@ const codes = [
   "a"
 ];
 function init() {
+  // set index to 0 so you can control it throughout
   let index = 0;
+  // added event listener to the body of the document and listened for keys pressed
   document.body.addEventListener('keydown', function(event) {
+    // if the key pressed matched the codes array
     if (event.key === codes[index]) {
+      // if all 10 keys are pressed, message displays
       if (index === 9) {
-        alert('Bingo!!!')
+        alert('Congrats, you have accomplished the Konami Code!!')
+      // if not you add one to the index
       } else {
       index += 1;
       }
+    // if they key does not match it sets the index back to 0 to start over  
     } else {
       index = 0;
     }
   });
 }
-init();
+
